a9efd459390badd7acaad4b523332dcde9a9d780
Michal Hladik <michalhladik2@seznam.cz>
2023-02-06 17:04:20 +0100

    tailwind on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <>
-      <h1>Green Bay</h1>
-      <p>Here you would see listed items</p>
-    </>
+    <div className="p-8 h-screen bg-gray-300">
+      <h1 className="text-6xl bg-green-200 my-2 px-2 py-4 text-green-800 font-bold underline">
+        Green Bay
+      </h1>
+      <p className="text-xl font-medium">Here you would see listed items</p>
+    </div>
   )
 }
