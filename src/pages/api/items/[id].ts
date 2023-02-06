c020f0ad6195ba07ba8cbed19e07ba2de7efba41
Michal Hladik <michalhladik2@seznam.cz>
2023-02-06 16:41:13 +0100

    more information for api endpoints

--- src/pages/api/items/[id].ts
+++ src/pages/api/items/[id].ts
@@ -11,5 +11,7 @@ export default function handler(
   //todo: GET to return single item information: name, description, photo url, price, sellers name and if bought also buyer's name
 
   //todo: POST to buy an item
+  // only when logged user has enough green dollars
+  // on successfully buying item, subtract amount from buyer's dollars, mark the item as sold and add buyer's name
   res.status(200).json({ name: 'John Doe' })
 }
--- src/pages/api/items/index.ts
+++ src/pages/api/items/index.ts
@@ -10,6 +10,11 @@ export default function handler(
 ) {
   //todo: GET to return all sellable items with name, photo url and price OR nothing if such item doesn't exists
 
-  //todo: POST to buy an item
+  //todo: POST to add new item
+  // needs name, description, photo url and price
+  // price is positive integer
+  // photo url is valid url
+  // has all the information
+  // save it to database and application shows newly created item
   res.status(200).json({ name: 'John Doe' })
 }
