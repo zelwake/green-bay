c04632fd49e9b6acd0903c80b9d9a4ffd722185c
Michal Hladik <michalhladik2@seznam.cz>
2023-02-14 20:14:44 +0100

    few more api tests for items/index

--- src/__test__/api/items/index.test.ts
+++ src/__test__/api/items/index.test.ts
@@ -70,7 +70,7 @@ describe('/api/items/index GET API endpoint', () => {
   })
 })
 
-describe('/api/items/index GET API endpoint', () => {
+describe('/api/items/index POST API endpoint', () => {
   function mockRequestResponse(method: RequestMethod = 'POST') {
     const {
       req,
@@ -95,12 +95,75 @@ describe('/api/items/index GET API endpoint', () => {
   it('should return 401 when unauthorized', async () => {
     ;(checkToken as jest.Mock).mockImplementation(() => null)
     const { req, res } = mockRequestResponse()
+
     await handler(req, res)
 
     expect(res.statusCode).toEqual(401)
   })
 
   jest.mock('@/scripts/checkToken', () => jest.fn(() => true))
+
+  it('should return 400 status code, error missing parameters', async () => {
+    ;(checkToken as jest.Mock).mockImplementation(() => true)
+    ;(databaseQuery as jest.Mock).mockImplementation(() => ({ insertId: 1 }))
+    const { req, res } = mockRequestResponse()
+    req.body = {
+      name: '',
+      description: '',
+      photoUrl: '',
+      price: '',
+    }
+    await handler(req, res)
+
+    expect(res.statusCode).toEqual(400)
+    expect(res.getHeaders()).toEqual({ 'content-type': 'application/json' })
+    expect(res._getJSONData()).toEqual({
+      error: 'Missing one or more information',
+    })
+  })
+
+  it('should return 400 status code, error positive integer', async () => {
+    ;(checkToken as jest.Mock).mockImplementation(() => true)
+    ;(databaseQuery as jest.Mock).mockImplementation(() => ({ insertId: 1 }))
+    const { req, res } = mockRequestResponse()
+    const numbers = ['0', '-1', '1.1'] //todo: throwing error
+    // for (const number of numbers) {
+    req.body = {
+      name: 'Test',
+      description: 'Test of api',
+      photoUrl: 'http://image.png',
+      price: '1.1',
+      // price: number,
+    }
+    console.log(req.body)
+    await handler(req, res)
+
+    expect(res.statusCode).toEqual(400)
+    expect(res.getHeaders()).toEqual({ 'content-type': 'application/json' })
+    expect(res._getJSONData()).toEqual({
+      error: 'Price must be positive integer',
+    })
+    // }
+  })
+
+  it('should return 400 status code, error wrong url', async () => {
+    ;(checkToken as jest.Mock).mockImplementation(() => true)
+    ;(databaseQuery as jest.Mock).mockImplementation(() => ({ insertId: 1 }))
+    const { req, res } = mockRequestResponse()
+    req.body = {
+      name: 'Test',
+      description: 'Test of api',
+      photoUrl: 'www.image.png',
+      price: 123,
+    }
+    await handler(req, res)
+
+    expect(res.statusCode).toEqual(400)
+    expect(res.getHeaders()).toEqual({ 'content-type': 'application/json' })
+    expect(res._getJSONData()).toEqual({
+      error: 'Url must be valid',
+    })
+  })
 
   it('should return 200 status code', async () => {
     ;(checkToken as jest.Mock).mockImplementation(() => true)
@@ -112,6 +175,30 @@ describe('/api/items/index GET API endpoint', () => {
     expect(res.getHeaders()).toEqual({ 'content-type': 'application/json' })
     expect(res._getJSONData()).toEqual({
       item: 1,
+    })
+  })
+})
+
+describe('/api/items/index PUT API endpoint', () => {
+  function mockRequestResponse(method: RequestMethod = 'PUT') {
+    const {
+      req,
+      res,
+    }: {
+      req: NextApiRequest & ReturnType<typeof createRequest>
+      res: NextApiResponse & ReturnType<typeof createResponse>
+    } = createMocks({ method })
+    return { req, res }
+  }
+
+  it('should return 405 when not calling GET or POST', async () => {
+    ;(checkToken as jest.Mock).mockImplementation(() => true)
+    const { req, res } = mockRequestResponse()
+    await handler(req, res)
+
+    expect(res.statusCode).toEqual(405)
+    expect(res._getJSONData()).toEqual({
+      error: 'Method not allowed',
     })
   })
 })
